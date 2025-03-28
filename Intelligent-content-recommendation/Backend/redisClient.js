102cfcfb85d26ef1e5daf8dd59c130a05a877352
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-03-28 22:14:34 +0100

    Update redisClient.js

--- Intelligent-content-recommendation/Backend/redisClient.js
+++ Intelligent-content-recommendation/Backend/redisClient.js
@@ -1,8 +1,8 @@
 const Redis = require('ioredis');
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || '127.0.0.1',
-  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_HOST || '127.0.0.1', //Replace with your Redis server host
+  port: process.env.REDIS_PORT || 6379,        // Replace with your Redis server port
   password: process.env.REDIS_PASSWORD || undefined,
 });
 
