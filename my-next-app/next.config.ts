cfdea7098154e78ebda78f936c9053b18948e35e
Christopher Oluwatobi <Christopher.Oluwatobi@Sterling.ng>
2025-04-02 07:20:38 +0100

    initial commit

--- my-next-app/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // No 'builds' configuration present
-};
-
-export default nextConfig;
--- my-next-app/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-// pages/index.js
-export default function Home() {
-    return <h1>Welcome to Intelligent Content Recommendation System</h1>;
-}
-// This is a simple Next.js page that serves as the homepage of your application.
-// It exports a functional component that returns a heading element with a welcome message.
